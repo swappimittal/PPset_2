a50bf3f1683861d547c67541aa109030c1b76127
swappimittal <148274152+swappimittal@users.noreply.github.com>
2023-10-23 13:15:09 -0700

    Update streamlit_probe_app.py

--- streamlit_probe_app.py
+++ streamlit_probe_app.py
@@ -392,11 +392,7 @@ def get_missmatch_values(probe_para_dict,variant, token):
         comp_seq = Seq(PROBE).complement()
         
         # Construct the mismatch sequence
-        mismatch_seq = (
-            comp_seq[:snp_pos - 1] +
-            Seq(variant).complement() +
-            comp_seq[snp_pos:]
-        )
+        mismatch_seq = (comp_seq[:snp_pos - 1] + Seq(variant).complement() + comp_seq[snp_pos:])
         #get_mismatch_from_IDT(probe_seq, mismatch_seq, token)
         # Use the get_mismatch_from_IDT function to fetch the mismatch value
         probe_para_dict[probe]["Tm miss"] =  mismatch_seq         
